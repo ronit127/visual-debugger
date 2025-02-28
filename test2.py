b0cb7f635b7face1a829fe6bb5ca0b018e659dd5
ronit127 <00ronitr@gmail.com>
2025-02-28 13:30:00 -0600

    another update to test2

--- test2.py
+++ test2.py
@@ -3,18 +3,24 @@ import io
 import sys
 import re
 
-# take in some code, convert into a form python understands, and executes it
+# take in some code, and executes it
 def execute_code(code):
-    print("executing code")
+    print("executing code...")
     exec(code) # TODO: figure out how to collect errors and print them without stopping our program
-    status = 0
     
-    return status, string
+    status = "" # TODO: change status according to either the output or the run-time / syntax errors of the code (should use stdout redirect or similar)
 
-# take in some code, give a list of lines that are run
+    #TODO: Come up with ways to safely execute the code (prevent malicious code)
+    return status
+
+# take in some code, give a list of lines in the order they are run when executed.
+# this allows us to know the order of lines we need to highlight and more importantly, process and visualize 
 def trace_code(code):
-    #TODO: use the trace function to go through the input code
+    #TODO: use the trace function to go through the input code (perhaps the Trace() and run() functions among others)
+
+    #TODO: return a list of tuples in the form (n, x) where n is the line number and x is the corresponidng line of code (String)
     pass
+
 
 code = """
 def test_function():
