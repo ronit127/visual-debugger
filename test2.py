a66465520570d6e4103529ef5529af4f646668a2
aryaarathi <aryaar2@illinois.edu>
2025-03-12 23:48:32 -0500

    Completed trace_code

--- test2.py
+++ test2.py
@@ -19,7 +19,34 @@ def trace_code(code):
     #TODO: use the trace function to go through the input code (perhaps the Trace() and run() functions among others)
 
     #TODO: return a list of tuples in the form (n, x) where n is the line number and x is the corresponidng line of code (String)
-    pass
+    
+    exec_trace = []
+    exec_order = 0
+    lines = code.split("\n")
+    code_map = {}
+    for i in range(len(lines)):
+        code_map[i+1] = lines[i]
+
+    def trace_func(frame, event, arg):
+        nonlocal exec_order
+        if (event == "line"):
+            line_no = frame.f_lineno
+            if line_no in code_map:
+                exec_order += 1
+                exec_trace.append((exec_order, code_map[line_no].strip()))
+        return trace_func
+        
+    compiled = compile(code, "<string>", "exec")
+
+    sys.settrace(trace_func)
+
+    try:
+        exec(compiled, {})
+    finally:
+        sys.settrace(None)
+    
+    return exec_trace
+
 
 
 # breakpoints: list of breakpoints set by the user
@@ -38,10 +65,13 @@ def test_function():
     x = 10
     y = 20
     for i in range(0, 5):
-    x += 10
+        x += 10
     return x+y
     
 print(test_function())
 """
 
-process_code(code)
+#process_code(code)
+
+tuples = trace_code(code)
+print(tuples)
