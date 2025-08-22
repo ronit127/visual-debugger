33e4f061e9d324f39b743047ccc2d483b05543c1
ronit127 <00ronitr@gmail.com>
2025-08-22 18:20:16 -0500

    removed legacy files

--- testing.py
+++
@@ -1,8 +0,0 @@
- # with contextlib.redirect_stdout(output):(maybe)
-    # try:
-    #     pass
-    # except Exception as e:
-    #     return jsonify({
-    #         'status': 'error',
-    #         'error': str(e)
-    #     })
