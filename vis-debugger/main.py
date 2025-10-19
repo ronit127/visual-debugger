39b1a0be4e121feab3b129962eaf418f859ee66f
Rimma Berezhnaia <rimmab2@illinois.edu>
2025-10-19 16:54:50 -0500

    added lists to correct main

--- vis-debugger/main.py
+++ vis-debugger/main.py
@@ -278,3 +278,56 @@ def removeEdge(graph, node1st, node2nd):
 def removeNode(graph, node_r):
     """Log node removal operation."""
     graph_operations.append(f"Graph '{graph}': Node removed with value: {node_r}")
+
+
+
+c = """
+l = [1, 2, 3, 4]
+l.append(1)
+l.pop()
+"""
+
+
+
+def list_parse(code: str):
+    tree = ast.parse(code)
+    list_vars = set()
+
+# walking throuhg ast nodes
+# need class for bc ast works with classes that extend NodeVisitor
+    class Functions(ast.NodeVisitor):
+        #whenever x= is found
+        def visit_Assign(self, node):
+            # checks if it is a list assignment
+            if isinstance(node.value, ast.List):
+                # if a list adds the variable names
+                for target in node.targets:
+                    if isinstance(target, ast.Name):
+                        list_vars.add(target.id)
+                        for elt in node.value.elts:
+                        # call the visualization add
+                            print("adding existing element") # call the visualization add
+            #self.generic_visit(node) # keep going through the rest of the tree under this node so nothing else gets missed
+           
+
+
+        def visit_Call(self, node):
+            # check if method call on variable
+            if isinstance(node.func, ast.Attribute) and isinstance(node.func.value, ast.Name):
+                var_name = node.func.value.id
+                method_name = node.func.attr
+                if var_name in list_vars:
+                    if method_name == 'append'or method_name == 'push':
+                        print("adding a node") # call tje visualization add
+                    elif method_name == 'pop':
+                        print("deleting a node") # call the visualization delete
+            #self.generic_visit(node)
+    #visit_assign and visit_call get called automatically by the NodeVisitor
+    Functions().visit(tree)
+
+
+list_parse(c)
+
+
+
+
