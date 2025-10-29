24baf61626836fee1a2f327852a435924cb332d8
Felix <105163888+fishgod8@users.noreply.github.com>
2025-10-28 20:01:43 -0500

    added ability to close window

--- vis-debugger/react/visual-debugger/app/page.tsx
+++ vis-debugger/react/visual-debugger/app/page.tsx
@@ -10,7 +10,10 @@ import { Rnd } from "react-rnd";
 
 function DraggableComponent() {
   const [visible, setVisible] = useState(true);
-  const [isMaximized, setIsMaximized] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  if (!visible) return null;
+
   return (
     <Rnd
       default={{
@@ -22,33 +25,57 @@ function DraggableComponent() {
       minHeight="100"
       minWidth="150"
       bounds="window"
-      dragHandleClassName="draggable"
-      enableResizing={{
-        bottom: true,
-        bottomLeft: true,
-        bottomRight: true,
-        left: true,
-        right: true,
-        top: true,
-        topLeft: true,
-        topRight: true,
-      }}
+      dragHandleClassName="window-title"
+      style={{ borderRadius: "10px", overflow: "hidden" }}
     >
-      <div className="draggable" style={{ display: "flex" }}>
-        <div
-          style={{
-            position: "relative",
-            width: "100%",
-            height: "100%",
-            boxSizing: "border-box",
-            padding: "20px",
-            background: "#f0f0f0",
-            border: "1px solid #ccc",
-            color: "black",
-          }}
-        >
-          I&apos;m a draggable window!
+      <div
+        className="window-title"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          height: "32px",
+          padding: "0 6px",
+          backgroundColor: "#3c3c3cff",
+        }}
+      >
+        <div style={{ display: "flex", gap: "8px" }}>
+          {/* close window */}
+          <div
+            id="close"
+            style={{
+              width: 12,
+              height: 12,
+              background: "#ff5f56", // red
+              borderRadius: "50%",
+              cursor: "pointer",
+              color: "grey",
+              fontWeight: "bold",
+              fontSize: "12px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            onClick={() => setVisible(false)}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            {isHovered ? "x" : ""}
+          </div>
         </div>
+      </div>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          boxSizing: "border-box",
+          padding: "20px",
+          background: "#f0f0f0",
+          border: "1px solid #ccc",
+          color: "black",
+        }}
+      >
+        I&apos;m a draggable, closeable, resizeable window!
       </div>
     </Rnd>
   );
