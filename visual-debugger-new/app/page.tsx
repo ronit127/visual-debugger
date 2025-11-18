b48c5ad65695a288c1419d1312bd937bec6e792a
Felix <105163888+fishgod8@users.noreply.github.com>
2025-11-17 21:56:35 -0600

    experimenting with react flow for use of connections, edges, and nodes to connect draggable components with editor

--- visual-debugger-new/app/page.tsx
+++ visual-debugger-new/app/page.tsx
@@ -3,6 +3,9 @@
 import React, { useState } from "react";
 import CodeEditor from "./components/editor";
 import DraggableComponent from "./components/draggable";
+// experimenting with React Flow library for connections between editor and draggable components
+import { ReactFlow } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
 export default function App() {
   const [code, setCode] = useState<string>(
@@ -10,29 +13,38 @@ export default function App() {
   );
 
   return (
-    <>
-      <DraggableComponent zIndex={20} />
-      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex min-h-screen w-full max-w-4xl flex-col items-center justify-start py-12 px-6 bg-white dark:bg-black sm:items-start">
-          <div className="w-full">
-            <div className="mb-6">
-              <h1 className="text-3xl font-semibold text-black dark:text-zinc-50">
-                Visual Debugger
-              </h1>
-              <p className="text-zinc-600 dark:text-zinc-400">
-                A simple code editor for now....
-              </p>
-            </div>
-
-            <CodeEditor
-              code={code}
-              onChange={setCode}
-              height="70vh"
-              zIndex={10}
-            />
-          </div>
-        </main>
+    <div className="relative h-screen w-screen">
+      {/* React Flow Canvas*/}
+      <div className="absolute inset-0 z-20">
+        <ReactFlow style={{ width: "100%", height: "100%" }}>
+          <DraggableComponent zIndex={40} />
+          {/* make draggable component node and experiment with react flow */}
+        </ReactFlow>
       </div>
-    </>
+      {/* Editor and stuff */}
+      <div className="relative z-30">
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+          <main className="flex min-h-screen w-full max-w-4xl flex-col items-center justify-start py-12 px-6 bg-white dark:bg-black sm:items-start">
+            <div className="w-full">
+              <div className="mb-6">
+                <h1 className="text-3xl font-semibold text-black dark:text-zinc-50">
+                  Visual Debugger
+                </h1>
+                <p className="text-zinc-600 dark:text-zinc-400">
+                  A simple code editor for now....
+                </p>
+              </div>
+
+              <CodeEditor
+                code={code}
+                onChange={setCode}
+                height="70vh"
+                zIndex={10}
+              />
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
   );
 }
